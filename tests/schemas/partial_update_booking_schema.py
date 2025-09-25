b2be8646a699a4ffd30edf19a4d3e308ad933959
antonvarfolomeev <varfolomeev2202@gmail.com>
2025-09-25 14:13:39 +0300

    Added methods for update and partial update booking + json schema for validating response

---
+++ tests/schemas/partial_update_booking_schema.py
@@ -0,0 +1,21 @@
+PARTIAL_UPDATING_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "firstname": {"type": "string"},
+        "lastname": {"type": "string"},
+        "totalprice": {"type": "integer"},
+        "depositpaid": {"type": "boolean"},
+        "bookingdates": {
+            "type": "object",
+            "properties": {
+                "checkin": {"type": "string", "format": "date"},
+                "checkout": {"type": "string", "format": "date"}
+            },
+            "required": [],
+            "additionalProperties": False
+            },
+        "additionalneeds": {"type": "string"}
+    },
+    "additionalProperties": False,
+    "required": []
+}
